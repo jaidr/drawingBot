5732489a0586e318db3a7de7f089d29900fc373f
jaidr <118378713+jaidr@users.noreply.github.com>
2022-12-04 09:29:41 -0600

    Update White background.py

--- White background.py
+++ White background.py
@@ -3,6 +3,7 @@ import keyboard
 
 # go from left to right while mouse down (on cooldown)
 # go down 6 pixels
+# hard coded example to speed up 'blanking'/ canvas creation process for my computer
 for y in range(34):
     if keyboard.is_pressed("b"):
         break
